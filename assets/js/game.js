4f8152095335c3bd689d0f2bd3a8c132d7728d91
SiriusScaper <siriusscaper@gmail.com>
2018-02-09 11:06:00 -0800

    Jaseat - Added all main functionality for game logic object and separate interface object

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,18 +3,19 @@ var game = {
 			"second test",
 			"final test"],
 	currentWord: "",
-	guesses: [];
-	guessCount: 0
+	guesses: [],
+	guessCount: 0,
 	guessWord: [],
 	winCount,
 	key: null,
 
 	begin: function(){
-		this.guesses = 10;
 		this.setWord();
+		this.winCount = 0;
 	},
-
+	
 	setWord: function(){
+		this.guessCount = 10;
 		this.currentWord = this.words[Math.floor(Math.random()*this.words.length)];
 		for(var i = 0; i < currentWord.length; i++){
 			if(this.currentWord[i] == " "){
@@ -24,11 +25,29 @@ var game = {
 				this.guessWord[i] = "_";
 		}
 
-	}
+	},
 
 	update: function(){
 		//update the game
-	}
+		if(this.guesses.indexOf(key) != -1){
+			return 0;
+		} 
+		var index = this.currentWord.indexOf(key);
+		if (index === -1) {
+			--this.guessCount;
+		} else {
+			this.guessWord[index] = this.currentWord[index];
+		}
+		if (this.currentWord === this.guessWord.join('')) {
+			++this.winCount;
+			this.setWord();
+		} else if(this.guessCount <= 0) {
+		}
+	},
+	// need a print function
+		// call interface print function
+		// each var changed that matches a dom element (div with ID)
+		// call at end of setword and update
 
 }
 
@@ -40,6 +59,6 @@ var interface = {
 		game.update();
 	}
 }
-
+// function for printing to inner html taking html and string as arguements
 
 document.onkeyup = interface.keyup;
